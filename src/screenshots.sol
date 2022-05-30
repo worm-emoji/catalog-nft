cace5fbbadf9a06c46d5b9fb42253e3ec4c1db2f
luke miles <luke@lukemil.es>
2022-05-29 21:48:11 -0700

    checkin

--- src/screenshots.sol
+++ src/screenshots.sol
@@ -140,6 +140,6 @@ contract screenshots is Ownable, ERC721 {
             interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
             interfaceId == 0x5b5e139f || // ERC165 Interface ID for ERC165
             interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC721Metadata
-            interfaceId == 0x2a55205a; // ERC165 Interface ID for ERC2981
+            interfaceId == 0x2a55205a; // ERC165 Interface ID for https://eips.ethereum.org/EIPS/eip-2981
     }
 }
