9a11566da30d0b673ea86547b3ad8d495aab3dae
luke miles <luke@lukemil.es>
2022-05-30 23:03:35 -0700

    center

--- frontend/pages/s/[id].tsx
+++ frontend/pages/s/[id].tsx
@@ -42,7 +42,7 @@ function Post({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
       </Head>
       <div className="flex w-100 justify-center">
         <div className="px-2 py-5 flex flex-col justify-center items-center w-100 max-w-xl text-white ">
-          <h2 className="text-2xl">{post.name}</h2>
+          <h2 className="text-2xl text-center">{post.name}</h2>
         </div>
       </div>
       <Screenshot key={post.id} data={post} isIndex />
