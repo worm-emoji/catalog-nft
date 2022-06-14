86b97234e85a151451fb1c7df68dd731b7728fe8
luke miles <luke@lukemil.es>
2022-06-14 16:17:19 -0700

    update price

--- frontend/components/Screenshot.tsx
+++ frontend/components/Screenshot.tsx
@@ -15,7 +15,7 @@ import abi from '../eth/abi.json'
 import Link from 'next/link'
 
 const zero = '0x0000000000000000000000000000000000000000'
-const price = '0.08'
+const price = '0.02'
 
 function substrAddress(address: string) {
   return address.substr(0, 6) + '...' + address.substr(address.length - 4)
