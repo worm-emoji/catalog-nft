7d33c8c8aa7e6f6f710b8e663044ee5e27200368
Malone Hedges <malonehedges@users.noreply.github.com>
2022-11-02 23:14:34 -0700

    sign env

--- frontend/data/sign.ts
+++ frontend/data/sign.ts
@@ -2,9 +2,7 @@ import { ethers } from 'ethers'
 import { keccak256 } from '@ethersproject/keccak256'
 import { env } from 'process'
 
-const key = new ethers.utils.SigningKey(
-  '0x0123456789012345678901234567890123456789012345678901234567890123',
-)
+const key = new ethers.utils.SigningKey(env.SIGNING_KEY || '')
 const coder = new ethers.utils.AbiCoder()
 
 export const getSignature = (id: number): string => {
