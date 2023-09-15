f5954d186b24c9cf365157db446ebfdf9942ba0b
luke miles <luke@lukemil.es>
2023-09-14 22:04:49 -0700

    fix images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['luke.cat', 'blotcdn.com', 'blot.im'] },
+  images: { domains: ['luke.cat', 'blotcdn.com', 'blot.im', 'cdn.blot.im'] },
   experimental: { images: { allowFutureImage: true } },
 }
 
